53a6764a3ebf134f5a5a639ee604783e355630e8
cameron.townsend <13910756+goldcam@users.noreply.github.com>
2025-07-06 08:32:44 -0700

    Set up basic stopwatch

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,68 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script lang="ts">
+
+    interface TimerObj {
+        sec: number;
+        min: number;
+        hour: number;
+    }
+
+    const timerObj: TimerObj = {
+        sec:0, 
+        min:0,
+        hour:0
+    };
+
+
+    let count: number = 0,
+        isRunning: boolean = false,
+        timer: number, //for interval
+        cycles: number = 0,
+        preCountDown: number = 0,
+        totalTime: number;
+    
+
+    const tabataTimer = ():void => {
+        isRunning = !isRunning;
+        if(isRunning) {
+            timer = setInterval(() => {
+            timerObj.sec++;
+            if (timerObj.sec === 60) {
+                timerObj.sec = 0;
+                timerObj.min++;
+                if (timerObj.min === 60) {
+                    timerObj.min = 0;
+                    timerObj.hour++;
+                }
+            }
+        }, 1000);
+        } else {
+            clearInterval(timer);
+        }
+
+    }
+    const resetTabata = ():void => {
+        isRunning = false;
+        timerObj.sec = 0;
+        timerObj.min = 0;
+        timerObj.hour = 0;
+
+    }
+
+</script>
+
+
+<div class="main">
+    <div class="clockWrap">
+        {count}
+        <div id="display">{timerObj.hour} {timerObj.min} : {#if timerObj.sec < 10} 0 {/if} {timerObj.sec}</div>
+         <button id="Btn" on:click={tabataTimer}>
+            {#if isRunning}
+                Pause
+            {:else}
+               Start
+            {/if}
+        </button>
+        <button id="resetBtn" on:click={resetTabata}>Reset</button>
+    </div>
+</div>
+
