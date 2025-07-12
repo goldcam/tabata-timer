3abb1aec55da639104e7907440674109d8e07d58
cameron.townsend <13910756+goldcam@users.noreply.github.com>
2025-07-11 17:06:10 -0700

    Move logic out of html

--- src/lib/components/statusDisplay/statusDisplay.svelte
+++ src/lib/components/statusDisplay/statusDisplay.svelte
@@ -2,34 +2,33 @@
     import state from '../../state/state.svelte';
     import { STATUS_TEXT } from '../../constants/constants.svelte';
 
-
-
+    let { currentPhase } = $props();
 
     // export let currentPhase: 'pre' | 'work' | 'rest' | 'paused' | 'done';
 
-    // const statusText = $derived(() => {
+    const statusText = $derived.by(() => {
+        switch (currentPhase) {
+            default:
+            case 'pre': return STATUS_TEXT.PRE;
+            case 'work': return STATUS_TEXT.WORK;
+            case 'rest': return STATUS_TEXT.REST;
+            case 'paused': return STATUS_TEXT.PAUSE;
+            case 'done': return STATUS_TEXT.DONE;            
+        }
+    });
+    // const statusText = () => {
     //     switch (currentPhase) {
+    //         default:
     //         case 'pre': return STATUS_TEXT.PRE;
     //         case 'work': return STATUS_TEXT.WORK;
     //         case 'rest': return STATUS_TEXT.REST;
     //         case 'paused': return STATUS_TEXT.PAUSE;
-    //         case 'done': return STATUS_TEXT.DONE;
-    //         default: return '';
+    //         case 'done': return STATUS_TEXT.DONE;            
     //     }
-    // });
+    // };
 </script>
 
 
 <div class="status text-5xl" >             
-    {#if !state.isRunning && state.round === 0} 
-        {STATUS_TEXT.PRE} 
-    {:else if state.isWork && state.isRunning}
-        {STATUS_TEXT.WORK}
-    {:else if !state.isWork && state.isRunning}
-        {STATUS_TEXT.REST}
-    {:else if !state.isRunning && !state.isDone} 
-        {STATUS_TEXT.PAUSE}
-    {:else if state.isDone}
-        {STATUS_TEXT.DONE}             
-    {/if} 
+    {statusText}
 </div>
